8be94d7d32d7fad30c9e974df862197ec47f10fe
ShivDange <31200011+ShivDange@users.noreply.github.com>
2019-05-09 11:08:04 +0530

    Update index.js

--- Contents/index.js
+++ Contents/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Scene, Drawer } from 'react-native-router-flux';
 import { Icon } from 'react-native-material-ui';
-
+//import { Icon} from 'native-base';
 
 import {
   StyleSheet,
@@ -14,13 +14,14 @@ import ProfileView from './profile';
 import MapView from './map';
 import ChatView from './chat';
 
+
 class TabIcon extends Component {
   render() {
     const title = this.props.title;
     let icon = "";
     if (title == "Today") {
       icon = "today";
-
+      
 
     } else if (title == "Profile") {
       icon = "person";
@@ -30,8 +31,10 @@ class TabIcon extends Component {
       icon = "chat"
     }
     return (
-      <Icon name={icon} size={30}
-      />
+    
+      <Icon style={{color: this.props.focused ? '#81b71a' :'#00000'}} name={icon}></Icon>
+      
+    
     );
   }
 }
@@ -50,7 +53,11 @@ const App = () => {
           key="drawer"
           drawer={true}
           hideNavBar
-          barButtonIconStyle={{ tintColor: 'white' }}
+          
+          drawerImage={
+            require('../Icons/hamburger.png')
+            }
+
 
 
         >
@@ -58,8 +65,13 @@ const App = () => {
           <Scene
             key="tabbar"
             tabs={true}
-            tabBarStyle={{ backgroundColor: '#ffffff' }}
+            tabBarStyle={{ backgroundColor: '#ffffff'  }}
+            labelStyle = {
+              {color: 'black', 
+            }}
 
+          
+             
 
             hideNavBar
 
@@ -96,6 +108,7 @@ const App = () => {
 
 
 export default App;
+
 
 
 
